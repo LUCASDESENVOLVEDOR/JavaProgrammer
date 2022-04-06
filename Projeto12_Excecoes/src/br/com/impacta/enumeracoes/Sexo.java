2dce113d9fd5f470e82bfdfba903355d14e72db3
Lucas Aragão Feliciano <LUCAS_ARAGAO_FEL@HOTMAIL.COM>
2022-04-05 21:20:46 -0300

    Delete Projeto12_Excecoes directory

--- Projeto12_Excecoes/src/br/com/impacta/enumeracoes/Sexo.java
+++
@@ -1,6 +0,0 @@
-package br.com.impacta.enumeracoes;
-
-public enum Sexo {
-   MASCULINO, 
-   FEMININO  
-}
--- Projeto12_Excecoes/src/br/com/impacta/interfaces/Documento.java
+++
@@ -1,7 +0,0 @@
-package br.com.impacta.interfaces;
-
-public interface Documento {
-
-	void setNumero(String numero);
-	String getNumero();	
-}
