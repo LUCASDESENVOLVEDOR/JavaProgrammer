751695ed8ec21ab6c4f5bae5573dca2afda3a0d5
Lucas Aragão Feliciano <LUCAS_ARAGAO_FEL@HOTMAIL.COM>
2022-04-12 19:27:10 -0300

    Add files via upload

---
+++ Curso_Java/Projeto15_Arquivos/src/br/com/impacta/aplicacao/AppLeituraArquivo03.java
@@ -0,0 +1,56 @@
+package br.com.impacta.aplicacao;
+
+import java.io.FileReader;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+public class AppLeituraArquivo03 {
+
+	public static void main(String[] args) {
+		
+try {
+		
+		    JFileChooser chooser = new JFileChooser();
+		   
+		    if( chooser.showOpenDialog(null) == JFileChooser.CANCEL_OPTION) {
+		    	return;
+		    }	
+	
+			//Classe FileReader
+			FileReader  reader =  new FileReader(chooser.getSelectedFile()); 
+			
+			//leitura do arquivo um caractere por vez.			
+			
+			StringBuilder sb = new StringBuilder();		
+			
+			while(true) {
+				
+				int caractere = reader.read(); 
+				//System.out.println(caractere);
+				
+				//EOF = END OF FILE.
+				if(caractere == -1)
+					break;
+				
+				
+				char ch = (char)caractere;
+				sb.append(ch);				
+			}
+			
+			
+			System.out.println(sb);
+			reader.close();	
+				
+			
+			
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		}
+		
+		
+		
+
+	}
+
+}
